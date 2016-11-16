54f43ecaba96d6fef7fcda69b237ccf332a0b2b8
MaxxBit <massimiliano.vergnetta@gmail.com>
2016-11-16 15:17:40 +0100

    Initial commit

---
+++ src/main/java/com/stm/argo/web/ArgoController.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stm.argo.web;
+
+import com.stm.argo.service.CityService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ArgoController {
+
+	@Autowired
+	private CityService cityService;
+
+//	@GetMapping("/")
+//	@ResponseBody
+//	@Transactional(readOnly = true)
+//	public String helloWorld() {
+//		return this.cityService.getCity("Bath", "UK").getName();
+//	}
+
+	@RequestMapping("/city")
+	public String city(@RequestParam(value="name", required=false, defaultValue="Bath") String name, @RequestParam(value="country", required=false, defaultValue="UK") String country, Model model) {
+		model.addAttribute("name", this.cityService.getCity(name, country).getName());
+		return "city";
+	}
+
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+}
